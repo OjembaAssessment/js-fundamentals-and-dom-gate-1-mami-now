0b4f2c914234c0966b0a618e18b282cc18ad6a03
mamijose <mmammyj@gmail.com>
2023-07-24 11:31:59 +0200

    3rd question

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,26 @@
-console.log("Exercise 3 - Timer");
+const timer = document.getElementById('myTimer');
+let second=29;
+let addOnes=document.getElementById('addOne');
+addOnes.addEventListener('click',function(){
+  return second=second+1;
+});
+let minOnes=document.getElementById('minOne');
+five.addEventListener('click', function(){
+  return second=second-1;
+});
+let starts=document.getElementById('start');
+starts.addEventListener('click',function(){
+  return second=29;
+})
+let reset=document.getElementById('reset');
+reset.addEventListener('click',function(){
+  return second=0;
+  })
+
+let stopWatch= setInterval(function(){
+  if(second <= 0){
+    clearInterval(stopWatch);
+  }
+  timer.textContent=second;
+  second--
+},1000)
